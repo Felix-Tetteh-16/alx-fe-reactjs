1737869dc4d2341ce31b6529ff240c76cc2ae31f
Felix Tetteh <ftetteh182@gmail.com>
2026-01-11 20:53:23 +0000

    updatd the components of the app

---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <div>
+            <header>
+                <h1>My favorite cities</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent() {
+  return (
+    <div>
+      <main>
+        <p>
+          <p>I love to visit New York, Paris, and Tokyo.</p>
+        </p>
+      </main>
+    </div>
+  );
+}
+export default MainContent;
