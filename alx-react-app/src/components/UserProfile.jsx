8f82ab1f7cbea8bf2ff20ec729d04a3e9f826056
Felix Tetteh <ftetteh182@gmail.com>
2026-01-16 05:49:20 +0000

    made changes to the props component in the UserProfile

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
-const UserProfile = (props) => {
+const UserProfile = ({name, age, bio}) => {
   
   return (
     <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>Name: {name}</h2>
+      <p>Age: {age}</p>
+      <p>Bio: {bio}</p>
     </div>
   );
 };
